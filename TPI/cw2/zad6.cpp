6106970a9a8dddfe8d2d174b16a890362f0841fc
Jakub Ostrowicki <kuba.ostrowicki@interia.pl>
2014-10-26 19:16:20 +0100

    6

---
+++ TPI/cw2/zad6.cpp
@@ -0,0 +1,23 @@
+//fibonacci series
+//Assumption: it starts like this: 0 1 1 2 3 5
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int n;
+
+	cout<<"Give me index of the Fibonacci number you want to get: ";
+	cin>>n;
+
+	int tab[n];
+	tab[0] = 0;
+	tab[1] = 1;
+	for (int i=2; i<=n; i++){
+		tab[i] = tab[i-1] + tab[i-2];
+		cout<<"tab[i]: "<<tab[i]<<"***"<<i<<endl;
+	}
+	cout<<"Wynik: "<<tab[n-1]<<endl;
+
+	return 0;
+}
