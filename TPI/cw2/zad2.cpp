8d78e65b8ae0e470806bb3fc3d78fb3d610e5b53
Jakub Ostrowicki <kuba.ostrowicki@interia.pl>
2014-10-26 18:40:06 +0100

    2

---
+++ TPI/cw2/zad2.cpp
@@ -0,0 +1,37 @@
+//algorytm zachłanny, wydawanie reszty
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	float denominations[] = {20000, 10000, 5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1};
+	int amount[14];
+	float restZl;
+	
+	for (int i=0; i<14; i++){
+		amount[i]=0;
+	}
+
+	cout<<"How much cash is to be given back: ";
+	cin>>restZl;
+
+	float newRestZl = 100 * restZl;
+	int rest = int(newRestZl);
+	int a;
+	int i=0;
+	
+	while (rest>0 ){
+		a = rest / denominations[i];
+		rest -= a * denominations[i];
+		amount[i]=a;
+		i++;
+	}
+	cout<<"The cashier has to give following currencies: "<<endl;
+	for (int i=0; i<14; i++){
+		if (amount[i]!=0)
+			cout<<amount[i]<<" times the currency with value "<<denominations[i]/100<<" zl."<<endl;
+	}
+
+	return 0;
+}
